1561bce203d5cf655740916f56ef2339aa9d9634
kiminiuslt <kiminiuslt@gmail.com>
2022-04-24 18:09:05 +0300

    [CREATE] Created models

---
+++ src/main/java/eu/kiminiuslt/bdsm/model/Client.java
@@ -0,0 +1,9 @@
+package eu.kiminiuslt.bdsm.model;
+
+import eu.kiminiuslt.bdsm.model.enums.JobPost;
+
+public class Client {
+  private String username;
+  private String password;
+  private JobPost jobPost;
+}
