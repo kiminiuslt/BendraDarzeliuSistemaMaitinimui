1bb7c7e84df07ea9a7fb5508beb6badf6e02b5aa
kiminiuslt <kiminiuslt@gmail.com>
2022-04-26 12:04:43 +0300

    [ADD] Added save UUID, getList and edit options. Removed unused class

--- src/main/java/eu/kiminiuslt/bdsm/repository/ProductRepository.java
+++ src/main/java/eu/kiminiuslt/bdsm/repository/ProductRepository.java
@@ -3,15 +3,27 @@ package eu.kiminiuslt.bdsm.repository;
 import eu.kiminiuslt.bdsm.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class ProductRepository {
 
-  private List<Product> products = new ArrayList<>();
+  private Map<UUID, Product> products = new HashMap<>();
 
   public void save(Product product) {
-    products.add(product);
+    product.setUuid(UUID.randomUUID());
+    products.put(product.getUuid(), product);
+  }
+
+  public List<Product> getList() {
+    return new ArrayList<>(products.values());
+  }
+
+  public Product getProductByUUID(UUID id) {
+    return products.get(id);
+  }
+
+  public void update(Product product) {
+    products.put(product.getUuid(), product);
   }
 }
