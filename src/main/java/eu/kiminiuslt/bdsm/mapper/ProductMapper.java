5c032521cf5a86ca705044b776fb17057e9fc79c
kiminiuslt <kiminiuslt@gmail.com>
2022-05-03 12:47:55 +0300

    [REFACTOR] implemented new product FULL form by database requirements.

--- src/main/java/eu/kiminiuslt/bdsm/mapper/ProductMapper.java
+++ src/main/java/eu/kiminiuslt/bdsm/mapper/ProductMapper.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 public class ProductMapper {
   public ProductDto mapToProductDto(Product product) {
     return ProductDto.builder()
-        .kcal(product.getEnergyValueKcal())
-        .carbs(product.getCarbohydrates())
+        .energyValueKcal(product.getEnergyValueKcal())
+        .carbohydrates(product.getCarbohydrates())
         .fat(product.getFat())
-        .protein(product.getProteins())
+        .proteins(product.getProteins())
         .name(product.getName())
         .uuid(product.getUuid())
         .build();
