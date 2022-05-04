a34280c05ca38d676881d62f53a337b7362f4123
kiminiuslt <kiminiuslt@gmail.com>
2022-05-04 14:52:17 +0300

    [CREATE] implemented messages service

--- src/main/java/eu/kiminiuslt/bdsm/configs/ApplicationConfig.java
+++ src/main/java/eu/kiminiuslt/bdsm/configs/ApplicationConfig.java
@@ -32,7 +32,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
   }
 
   @Bean
-  public MessageSource messageSource(){
+  public MessageSource messageSource() {
     ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
     source.setBasename("classpath:messages");
     source.setDefaultEncoding("UTF-8");
---
+++ src/main/java/eu/kiminiuslt/bdsm/helpers/MessageService.java
@@ -0,0 +1,29 @@
+package eu.kiminiuslt.bdsm.helpers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class MessageService {
+  private final MessageSource messageSource;
+
+  public String getMessage(String key) {
+    try {
+      return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
+
+    } catch (NoSuchMessageException e) {
+      if (key != null && !key.equals("")) {
+        log.error("Key " + key + " does not exits");
+        e.printStackTrace();
+        return String.format("?%s?", key);
+      }
+    }
+    return "";
+  }
+}
