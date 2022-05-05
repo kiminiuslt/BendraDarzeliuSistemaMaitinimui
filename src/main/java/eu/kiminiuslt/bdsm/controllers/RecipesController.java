0e28521704c5883b18d86539007a169de2367ad6
kiminiuslt <kiminiuslt@gmail.com>
2022-05-05 16:52:16 +0300

    [ADD] RecipeService

--- src/main/java/eu/kiminiuslt/bdsm/controllers/RecipesController.java
+++ src/main/java/eu/kiminiuslt/bdsm/controllers/RecipesController.java
@@ -1,15 +1,31 @@
 package eu.kiminiuslt.bdsm.controllers;
 
+import eu.kiminiuslt.bdsm.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/recipes")
 public class RecipesController {
 
+  private final RecipeService recipeService;
+
   @GetMapping
-  public String recipesAll() {
-    return "recipie_all";
+  public String recipesAll(
+      Model model,
+      @PageableDefault(
+              size = 10,
+              sort = {"name"},
+              direction = Sort.Direction.ASC)
+          Pageable pageable) {
+    model.addAttribute("recipesListPages", recipeService.getAllRecipes(pageable));
+    return "recipe_all";
   }
 }
---
+++ src/main/java/eu/kiminiuslt/bdsm/service/RecipeService.java
@@ -0,0 +1,13 @@
+package eu.kiminiuslt.bdsm.service;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeService {
+
+    public Object getAllRecipes(Pageable pageable) {
+
+        return null;
+    }
+}
