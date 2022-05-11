e31001b7a1e51b0a4d5aac46db4c4a42eddf69d9
kiminiuslt <kiminiuslt@gmail.com>
2022-05-11 15:53:32 +0300

    [CREATE] new ProductsNamesDto and service to get it.

--- src/main/java/eu/kiminiuslt/bdsm/service/WarehouseService.java
+++ src/main/java/eu/kiminiuslt/bdsm/service/WarehouseService.java
@@ -1,6 +1,7 @@
 package eu.kiminiuslt.bdsm.service;
 
 import eu.kiminiuslt.bdsm.mapper.WarehouseMapper;
+import eu.kiminiuslt.bdsm.model.dto.ProductsNamesDto;
 import eu.kiminiuslt.bdsm.model.dto.WarehouseDto;
 import eu.kiminiuslt.bdsm.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class WarehouseService {
   private final WarehouseRepository warehouseRepository;
   private final WarehouseMapper warehouseMapper;
+  private final ProductService productService;
 
   public void addWarehouseRecord(WarehouseDto warehouseDto) {
     warehouseRepository.save(warehouseMapper.warehouseDtoToWarehouse(warehouseDto));
@@ -20,5 +24,9 @@ public class WarehouseService {
 
   public Page<WarehouseDto> getWarehouseList(Pageable pageable) {
     return warehouseRepository.findAll(pageable).map(warehouseMapper::warehouseToWarehouseDto);
+  }
+
+  public List<ProductsNamesDto> getAllProductsNames() {
+    return productService.getProductsListNamesDto();
   }
 }
