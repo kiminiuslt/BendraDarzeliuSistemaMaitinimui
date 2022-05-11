edda99f3f43f2af4770c102ad1f6c6bba02bb825
kiminiuslt <kiminiuslt@gmail.com>
2022-05-11 13:54:49 +0300

    [ADD] template all records table and service method getWarehouseList (pageable).

--- src/main/java/eu/kiminiuslt/bdsm/controllers/WarehouseController.java
+++ src/main/java/eu/kiminiuslt/bdsm/controllers/WarehouseController.java
@@ -1,7 +1,12 @@
 package eu.kiminiuslt.bdsm.controllers;
 
+import eu.kiminiuslt.bdsm.service.WarehouseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/warehouse")
 public class WarehouseController {
+  private final WarehouseService warehouseService;
 
-    @GetMapping
-    public String warehouseStart(){
-        return "/warehouse/warehouse-all";
-    }
+  @GetMapping
+  public String warehouseStart(
+      Model model,
+      @PageableDefault(
+              size = 15,
+              sort = {"productId"},
+              direction = Sort.Direction.ASC)
+          Pageable pageable) {
+    model.addAttribute("warehousePages", warehouseService.getWarehouseList(pageable));
+    return "/warehouse/warehouse-all";
+  }
 }
--- src/main/java/eu/kiminiuslt/bdsm/service/WarehouseService.java
+++ src/main/java/eu/kiminiuslt/bdsm/service/WarehouseService.java
@@ -4,6 +4,8 @@ import eu.kiminiuslt.bdsm.mapper.WarehouseMapper;
 import eu.kiminiuslt.bdsm.model.dto.WarehouseDto;
 import eu.kiminiuslt.bdsm.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Service;
 public class WarehouseService {
   private final WarehouseRepository warehouseRepository;
   private final WarehouseMapper warehouseMapper;
-  private final ProductService productService;
 
   public void addWarehouseRecord(WarehouseDto warehouseDto) {
     warehouseRepository.save(warehouseMapper.warehouseDtoToWarehouse(warehouseDto));
+  }
+
+  public Page<WarehouseDto> getWarehouseList(Pageable pageable) {
+    return warehouseRepository.findAll(pageable).map(warehouseMapper::warehouseToWarehouseDto);
   }
 }
