5fc23b8c0a29b9ecb0ca02cd63e72ef81e75c6f4
kiminiuslt <kiminiuslt@gmail.com>
2022-05-20 15:32:45 +0300

    [ADD] basic materials counting methods and template view.

--- src/main/java/eu/kiminiuslt/bdsm/model/dto/RecipeDto.java
+++ src/main/java/eu/kiminiuslt/bdsm/model/dto/RecipeDto.java
@@ -2,6 +2,8 @@ package eu.kiminiuslt.bdsm.model.dto;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,4 +18,36 @@ public class RecipeDto {
   private String recipeName;
   private String recipeText;
   private Set<ProductAndQuantityDto> productsList;
+
+  public Double allProteins() {
+    return round(
+        productsList.stream()
+            .mapToDouble(e -> (e.getProduct().getProteins() / 100) * e.getQuantity())
+            .sum());
+  }
+
+  public Double allFat() {
+    return round(
+        productsList.stream()
+            .mapToDouble(e -> (e.getProduct().getFat() / 100) * e.getQuantity())
+            .sum());
+  }
+
+  public Double allCarbohydrates() {
+    return round(
+        productsList.stream()
+            .mapToDouble(e -> (e.getProduct().getCarbohydrates() / 100) * e.getQuantity())
+            .sum());
+  }
+
+  public Double allEnergyValueKcal() {
+    return round(
+        productsList.stream()
+            .mapToDouble(e -> (e.getProduct().getEnergyValueKcal() / 100) * e.getQuantity())
+            .sum());
+  }
+
+  private double round(double value) {
+    return BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP).doubleValue();
+  }
 }
