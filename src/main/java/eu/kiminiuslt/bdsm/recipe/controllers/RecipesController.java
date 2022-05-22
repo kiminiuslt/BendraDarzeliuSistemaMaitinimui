a722ecc07fdc5730cca1aa475cd29329dee38031
kiminiuslt <kiminiuslt@gmail.com>
2022-05-22 17:10:32 +0300

    [REFACTOR] RecipesController split in two controllers (RecipesController AddProductController)

--- src/main/java/eu/kiminiuslt/bdsm/recipe/controllers/RecipesController.java
+++ src/main/java/eu/kiminiuslt/bdsm/recipe/controllers/RecipesController.java
@@ -1,7 +1,6 @@
 package eu.kiminiuslt.bdsm.recipe.controllers;
 
 import eu.kiminiuslt.bdsm.helpers.MessageService;
-import eu.kiminiuslt.bdsm.recipe.model.dto.ProductAndQuantityDto;
 import eu.kiminiuslt.bdsm.recipe.model.dto.RecipeDto;
 import eu.kiminiuslt.bdsm.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
@@ -50,47 +49,6 @@ public class RecipesController {
     recipeService.saveNameAndText(recipeDto);
     recipeService.addRecipe();
     return "redirect:/recipes/recipeForm?message=recipe.create.successes";
-  }
-
-  @GetMapping("/{uuid}/deleteProduct")
-  public String deleteProduct(@PathVariable("uuid") UUID uuid) {
-    recipeService.deleteProductFromRecipe(uuid);
-    return "redirect:/recipes/recipeForm";
-  }
-
-  @GetMapping("/{uuid1}/{uuid}/deleteProduct")
-  public String editingDeleteProduct(
-      @PathVariable("uuid") UUID uuid, @PathVariable("uuid1") UUID uuid1) {
-    recipeService.deleteProductFromRecipe(uuid);
-    return "redirect:/recipes/" + uuid1 + "/update";
-  }
-
-  @GetMapping("/addProduct")
-  public String addProduct(Model model) {
-    model.addAttribute("allProducts", recipeService.getAllProducts());
-    model.addAttribute("productAndQuantityDto", ProductAndQuantityDto.builder().build());
-    return "recipe/add-product";
-  }
-
-  @PostMapping("/addProduct")
-  public String addProduct(ProductAndQuantityDto productAndQuantityDto) {
-    recipeService.addProductToRecipe(productAndQuantityDto);
-    return "redirect:/recipes/recipeForm";
-  }
-
-  @GetMapping("/{uuid}/addProduct")
-  public String editRecipeAddProduct(Model model, @PathVariable("uuid") UUID uuid) {
-    model.addAttribute("allProducts", recipeService.getAllProducts());
-    model.addAttribute("productAndQuantityDto", ProductAndQuantityDto.builder().build());
-    model.addAttribute("recipeUUID", uuid);
-    return "recipe/add-product";
-  }
-
-  @PostMapping("/{uuid}/addProduct")
-  public String addProduct(
-      ProductAndQuantityDto productAndQuantityDto, @PathVariable("uuid") UUID uuid) {
-    recipeService.addProductToRecipe(productAndQuantityDto);
-    return "redirect:/recipes/" + uuid + "/update";
   }
 
   @GetMapping("/{uuid}/update")
