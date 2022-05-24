e773d66aa9cc74af3f0f73d04e1423b8f9076e7e
kiminiuslt <kiminiuslt@gmail.com>
2022-05-24 21:55:50 +0300

    [ADD] @PreAuthorize in controllers and @EnableGlobalMethodSecurity in SecurityConfig

--- src/main/java/eu/kiminiuslt/bdsm/recipe/controllers/AddProductController.java
+++ src/main/java/eu/kiminiuslt/bdsm/recipe/controllers/AddProductController.java
@@ -3,6 +3,7 @@ package eu.kiminiuslt.bdsm.recipe.controllers;
 import eu.kiminiuslt.bdsm.recipe.model.dto.ProductAndQuantityDto;
 import eu.kiminiuslt.bdsm.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('DIETIST')")
 @RequestMapping("/recipes")
 public class AddProductController {
 
--- src/main/java/eu/kiminiuslt/bdsm/recipe/controllers/RecipesController.java
+++ src/main/java/eu/kiminiuslt/bdsm/recipe/controllers/RecipesController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('DIETIST')")
 @RequestMapping("/recipes")
 public class RecipesController {
 
