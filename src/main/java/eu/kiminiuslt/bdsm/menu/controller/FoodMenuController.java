25bbdabc85c98a24d1719a3a3dc2467b51f53758
kiminiuslt <kiminiuslt@gmail.com>
2022-05-26 15:01:14 +0300

    [ADD] menu table in template. controller getMapping main table, and restrictions for roles.

--- src/main/java/eu/kiminiuslt/bdsm/menu/controller/FoodMenuController.java
+++ src/main/java/eu/kiminiuslt/bdsm/menu/controller/FoodMenuController.java
@@ -1,30 +1,23 @@
 package eu.kiminiuslt.bdsm.menu.controller;
 
-import eu.kiminiuslt.bdsm.menu.model.dto.MenuDto;
-import eu.kiminiuslt.bdsm.menu.model.entity.MenuDay;
 import eu.kiminiuslt.bdsm.menu.service.MenuService;
-import eu.kiminiuslt.bdsm.recipe.model.entity.Recipe;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 @Controller
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('DIETIST')")
 @RequestMapping("/food-menu")
 public class FoodMenuController {
   private final MenuService menuService;
 
   @GetMapping
-  public String foodMenu() {
-//    MenuDto menu = MenuDto.builder().daysList(new ArrayList<>()).build();
-//    menuService.save(menu);
-    menuService.delete();
+  public String foodMenu(Model model) {
+    model.addAttribute("menu", menuService.getMenu());
     return "/foodMenu/food-menu";
   }
 }
