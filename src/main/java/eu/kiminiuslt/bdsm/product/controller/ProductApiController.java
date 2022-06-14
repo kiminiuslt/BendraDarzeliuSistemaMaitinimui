08ccaf26c35f6662dfb49efcd4dbeb4c1ab46a9d
kiminiuslt <kiminiuslt@gmail.com>
2022-06-14 18:13:59 +0300

    [REFACTOR] getProducts refactored to getProductsPaginated and added create product method

--- src/main/java/eu/kiminiuslt/bdsm/product/controller/ProductApiController.java
+++ src/main/java/eu/kiminiuslt/bdsm/product/controller/ProductApiController.java
@@ -1,9 +1,12 @@
 package eu.kiminiuslt.bdsm.product.controller;
 
-import eu.kiminiuslt.bdsm.helpers.MessageService;
-import eu.kiminiuslt.bdsm.product.model.dto.ProductApiResponseDto;
+import eu.kiminiuslt.bdsm.product.model.dto.ProductDto;
 import eu.kiminiuslt.bdsm.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductApiController implements ProductApiDocumentation {
 
   private final ProductService productService;
-  private final MessageService messageService;
 
-  public ProductApiResponseDto getProducts() {
-    return ProductApiResponseDto.builder().products(productService.getProductsList()).build();
+  @Override
+  public Page<ProductDto> getProductsPaginated(int page, int size) {
+    return productService.getPageableProduct(PageRequest.of(page, size));
+  }
+
+  @Override
+  public ResponseEntity<Void> createProduct(ProductDto productDto) {
+    productService.addProduct(productDto);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 }
--- src/main/java/eu/kiminiuslt/bdsm/product/controller/ProductApiDocumentation.java
+++ src/main/java/eu/kiminiuslt/bdsm/product/controller/ProductApiDocumentation.java
@@ -1,25 +1,51 @@
 package eu.kiminiuslt.bdsm.product.controller;
 
-import eu.kiminiuslt.bdsm.product.model.dto.ProductApiResponseDto;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import eu.kiminiuslt.bdsm.product.model.dto.ProductDto;
+import io.swagger.annotations.*;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/products")
+import javax.validation.Valid;
+
+@RequestMapping("/api")
 @Api(tags = "Product Controller")
 public interface ProductApiDocumentation {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Get all products", notes = "Get all products from database")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(code = 200, message = "Successfully delivered list"),
-                    @ApiResponse(code = 401, message = "Authentication required to get data"),
-                    @ApiResponse(code = 403, message = "User don't have permission to access this data")
-            })
-    ProductApiResponseDto getProducts();
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  @ApiOperation(value = "Get products page", notes = "Chunk of all products list implemented by pagination")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Successfully delivered page"),
+        @ApiResponse(code = 401, message = "Authentication required to get data"),
+        @ApiResponse(code = 403, message = "User don't have permission to access this data")
+      })
+  Page<ProductDto> getProductsPaginated(
+      @ApiParam(
+              name = "page",
+              value = "Number of page",
+              type = "int",
+              example = "1",
+              required = true)
+          @RequestParam("page")
+          int page,
+      @ApiParam(
+              name = "size",
+              value = "Content size in page",
+              type = "int",
+              example = "5",
+              required = true)
+          @RequestParam("size")
+          int size);
+
+  @PostMapping("/create")
+  @ApiOperation(value = "Create product", httpMethod = "POST")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 201, message = "Successfully saved new product"),
+        @ApiResponse(code = 401, message = "Authentication required"),
+        @ApiResponse(code = 403, message = "User don't have permission to create product")
+      })
+  ResponseEntity<Void> createProduct(@Valid @RequestBody ProductDto productDto);
 }
