6a93f728b5ac047ee3bfccb9bc9c9c3ab842ddaf
kiminiuslt <kiminiuslt@gmail.com>
2022-06-28 14:23:52 +0300

    [ADD] FileResponse DTO

---
+++ src/main/java/eu/kiminiuslt/bdsm/core/files/dto/FileResponse.java
@@ -0,0 +1,11 @@
+package eu.kiminiuslt.bdsm.core.files.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class FileResponse {
+
+  private String originalFileName;
+}
