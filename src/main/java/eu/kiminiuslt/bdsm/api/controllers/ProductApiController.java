2bc6bece17545b2e5adfd8bf9b78dd41680dcba3
kiminiuslt <kiminiuslt@gmail.com>
2022-07-14 16:08:00 +0300

    [ADD] API endpoint to get all product names list

--- src/main/java/eu/kiminiuslt/bdsm/api/controllers/ProductApiController.java
+++ src/main/java/eu/kiminiuslt/bdsm/api/controllers/ProductApiController.java
@@ -1,20 +1,27 @@
 package eu.kiminiuslt.bdsm.api.controllers;
 
+import eu.kiminiuslt.bdsm.core.product.model.dto.ProductsNamesDto;
 import eu.kiminiuslt.bdsm.core.product.service.ProductService;
 import eu.kiminiuslt.bdsm.core.product.model.dto.ProductDto;
 import eu.kiminiuslt.bdsm.api.documentation.CrudApiDocumentation;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
+
+import static eu.kiminiuslt.bdsm.commons.Constants.AUTHENTICATION;
+import static eu.kiminiuslt.bdsm.commons.Constants.AUTHORIZATION;
 
 @RestController
 @RequiredArgsConstructor
@@ -53,5 +60,16 @@ public class ProductApiController implements CrudApiDocumentation<ProductDto> {
   public ResponseEntity<Void> delete(UUID uuid) {
     productService.deleteProduct(uuid);
     return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @GetMapping(path = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Successfully delivered list"),
+        @ApiResponse(code = 401, message = AUTHENTICATION),
+        @ApiResponse(code = 403, message = AUTHORIZATION)
+      })
+  List<ProductsNamesDto> getAllProductsNames() {
+    return productService.getProductsListNamesDto();
   }
 }
